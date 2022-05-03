7e8d2f220f4dd9edb18d8d0d177f9b35ec7bba46
Vic Khov <victorKhovTko@yandex.ru>
2022-05-04 02:55:48 +0300

    Started working on components

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+    import Keyboard from "./components/Keyboard.svelte";
+    import Board from "./components/Board.svelte";
+</script>
+
+
 <main>
     <nav>Wordle Clone</nav>
+    <Board />
+    <Keyboard />
 </main>
